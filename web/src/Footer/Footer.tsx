79edb8472b83c9768f0ec0f1b6ddea0da2a5d49d
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2020-03-13 21:18:19 +0100

    create header and footer dirs

---
+++ web/src/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.scss';
+import {Button, Container, Form, FormControl, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Container>
+        Footer
+      </Container>
+    </div>
+  );
+}
+
+export default Footer;
